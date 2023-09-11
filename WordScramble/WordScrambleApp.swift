9924a9c7dc3cfc679c0afc0bbe9c795eff3a2e5d
Novichenko Vitaliy <novichenkovitaliy@gmail.com>
2023-09-11 19:12:55 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Vitaliy Novichenko on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
